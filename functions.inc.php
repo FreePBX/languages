16e972794f15e2c378584d7b165bca2f56e862d2
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-12-02 05:53:34 +0000

    add language module that allows language change in the call flow and adds language attribute to extensions/users

--- functions.inc.php
+++ functions.inc.php
@@ -1,133 +1,229 @@
 <?php
-/*
-function miscapp_destinations() {
+
+function languages_destinations() {
 	// return an associative array with destination and description
-	foreach (announcement_list() as $row) {
-		$extens[] = array('destination' => 'app-announcement,ann-'.$row[0].',1', 'description' => $row[1]);
+	foreach (languages_list() as $row) {
+		$extens[] = array('destination' => 'app-languages,' . $row['language_id'] . ',1', 'description' => $row['description']);
 	}
 	return $extens;
 }
-*/
 
-function miscapps_contexts() {
-	// return an associative array with context and description
-	foreach (miscapps_list() as $row) {
-		$contexts[] = array(
-			'context' => 'app-miscapps-'.$row['miscapps_id'], 
-			'description'=> 'Misc Application: '.$row['description'],
-			'source' => 'Misc Applications',
-		);
-	}
-	return $contexts;
+function languages_getdest($exten) {
+	return array('app-languages,'.$exten.',1');
 }
 
-function miscapps_get_config($engine) {
+function languages_getdestinfo($dest) {
+	global $active_modules;
+
+	if (substr(trim($dest),0,14) == 'app-languages,') {
+		$exten = explode(',',$dest);
+		$exten = $exten[1];
+		$thisexten = languages_get($exten);
+		if (empty($thisexten)) {
+			return array();
+		} else {
+			$type = isset($active_modules['languages']['type'])?$active_modules['languages']['type']:'setup';
+			return array('description' => 'Language : '.$thisexten['description'],
+			             'edit_url' => 'config.php?display=languages&type='.$type.'&extdisplay='.urlencode($exten),
+								  );
+		}
+	} else {
+		return false;
+	}
+}
+
+function languages_get_config($engine) {
 	global $ext;
 	switch ($engine) {
 		case 'asterisk':
-			foreach (miscapps_list(true) as $row) {
-				if ($row['enabled']) {
-					$ext->add('app-miscapps-'.$row['miscapps_id'], $row['ext'], '', new ext_noop('Running miscapp '.$row['miscapps_id'].': '.$row['description']));
-					$ext->add('app-miscapps-'.$row['miscapps_id'], $row['ext'], '', new ext_goto($row['dest']));
-					
-					$ext->addInclude('from-internal-additional', 'app-miscapps-'.$row['miscapps_id']);
-				}
+			$ext->addInclude('from-internal-additional', 'app-languages');
+			foreach (languages_list() as $row) {
+					$ext->add('app-languages',$row['language_id'], '', new ext_noop('Changing Channel to language: '.$row['lang_code'].' ('.$row['description'].')'));
+					$ext->add('app-languages',$row['language_id'], '', new ext_setvar('LANGUAGE()',$row['lang_code']));
+					$ext->add('app-languages',$row['language_id'], '', new ext_goto($row['dest']));
 			}
 		break;
 	}
 }
 
-
-/**  Get a list of all miscapps
- * Optional parameter is get_ext. Potentially slow, because each row is extracted from the featurecodes table
- * one-by-one
+/**  Get a list of all languages
  */
-function miscapps_list($get_ext = false) {
+function languages_list() {
 	global $db;
-	$sql = "SELECT miscapps_id, description, dest FROM miscapps ORDER BY description ";
+	$sql = "SELECT language_id, description, lang_code, dest FROM languages ORDER BY description ";
 	$results = $db->getAll($sql, DB_FETCHMODE_ASSOC);
 	if(DB::IsError($results)) {
-		die_freepbx($results->getMessage()."<br><br>Error selecting from miscapps");	
+		die_freepbx($results->getMessage()."<br><br>Error selecting from languages");	
 	}
-	
-	if ($get_ext) {
-		foreach (array_keys($results) as $idx) {
-			$fc = new featurecode('miscapps', 'miscapp_'.$results[$idx]['miscapps_id']);
-			$results[$idx]['ext'] = $fc->getDefault();
-			$results[$idx]['enabled'] = $fc->isEnabled();
-		}
-	}
-	
 	return $results;
 }
 
-function miscapps_get($miscapps_id) {
+function languages_get($language_id) {
 	global $db;
-	$sql = "SELECT miscapps_id, description, ext, dest FROM miscapps WHERE miscapps_id = ".addslashes($miscapps_id);
+	$sql = "SELECT language_id, description, lang_code, dest FROM languages WHERE language_id = ".addslashes($language_id);
 	$row = $db->getRow($sql, DB_FETCHMODE_ASSOC);
 	if(DB::IsError($row)) {
-		die_freepbx($row->getMessage()."<br><br>Error selecting row from miscapps");	
+		die_freepbx($row->getMessage()."<br><br>Error selecting row from languages");	
 	}
 	
-	// we want to get the ext from featurecodes
-	$fc = new featurecode('miscapps', 'miscapp_'.$row['miscapps_id']);
-	$row['ext'] = $fc->getDefault();
-	$row['enabled'] = $fc->isEnabled();
-
 	return $row;
 }
 
-function miscapps_add($description, $ext, $dest) {
+function languages_add($description, $lang_code, $dest) {
 	global $db;
-	$sql = "INSERT INTO miscapps (description, ext, dest) VALUES (".
+	$sql = "INSERT INTO languages (description, lang_code, dest) VALUES (".
 		"'".addslashes($description)."', ".
-		"'".addslashes($ext)."', ".
+		"'".addslashes($lang_code)."', ".
 		"'".addslashes($dest)."')";
 	$result = $db->query($sql);
 	if(DB::IsError($result)) {
 		die_freepbx($result->getMessage().$sql);
 	}
-	//get id..
-	$miscapps_id = $db->getOne('SELECT LAST_INSERT_ID()');
-	if (DB::IsError($miscapps_id)) {
-		//TODO -- handle this
-	}
-	
-	$fc = new featurecode('miscapps', 'miscapp_'.$miscapps_id);
-	$fc->setDescription($description);
-	$fc->setDefault($ext, true);
-	$fc->update();
 }
 
-function miscapps_delete($miscapps_id) {
+function languages_delete($language_id) {
 	global $db;
-	$sql = "DELETE FROM miscapps WHERE miscapps_id = ".addslashes($miscapps_id);
+	$sql = "DELETE FROM languages WHERE language_id = ".addslashes($language_id);
 	$result = $db->query($sql);
 	if(DB::IsError($result)) {
 		die_freepbx($result->getMessage().$sql);
 	}
-	
-	$fc = new featurecode('miscapps', 'miscapp_'.$miscapps_id);
-	$fc->delete();
 }
 
-function miscapps_edit($miscapps_id, $description, $ext, $dest, $enabled=true) { 
+function languages_edit($language_id, $description, $lang_code, $dest) { 
 	global $db;
-	$sql = "UPDATE miscapps SET ".
+	$sql = "UPDATE languages SET ".
 		"description = '".addslashes($description)."', ".
-		"ext = '".addslashes($ext)."', ".
+		"lang_code = '".addslashes($lang_code)."', ".
 		"dest = '".addslashes($dest)."' ".
-		"WHERE miscapps_id = ".addslashes($miscapps_id);
+		"WHERE language_id = ".addslashes($language_id);
 	$result = $db->query($sql);
 	if(DB::IsError($result)) {
 		die_freepbx($result->getMessage().$sql);
 	}
-	
-	$fc = new featurecode('miscapps', 'miscapp_'.$miscapps_id);
-	$fc->setDescription($description);
-	$fc->setDefault($ext, true);
-	$fc->setEnabled($enabled);
-	$fc->update();
 }
 
+function languages_configpageinit($pagename) {
+	global $currentcomponent;
+
+	$action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
+	$extdisplay = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
+	$extension = isset($_REQUEST['extension'])?$_REQUEST['extension']:null;
+	$tech_hardware = isset($_REQUEST['tech_hardware'])?$_REQUEST['tech_hardware']:null;
+
+	// We only want to hook 'users' or 'extensions' pages.
+	if ($pagename != 'users' && $pagename != 'extensions') 
+		return true;
+	// On a 'new' user, 'tech_hardware' is set, and there's no extension. Hook into the page.
+	if ($tech_hardware != null || $pagename == 'users') {
+		language_applyhooks();
+		$currentcomponent->addprocessfunc('languages_configprocess', 5);
+	} elseif ($action=="add") {
+		// We don't need to display anything on an 'add', but we do need to handle returned data.
+		$currentcomponent->addprocessfunc('languages_configprocess', 5);
+	} elseif ($extdisplay != '') {
+		// We're now viewing an extension, so we need to display _and_ process.
+		language_applyhooks();
+		$currentcomponent->addprocessfunc('languages_configprocess', 5);
+	}
+}
+
+function language_applyhooks() {
+	global $currentcomponent;
+
+	// Add the 'process' function - this gets called when the page is loaded, to hook into 
+	// displaying stuff on the page.
+	$currentcomponent->addguifunc('languages_configpageload');
+}
+
+// This is called before the page is actually displayed, so we can use addguielem().
+function languages_configpageload() {
+	global $currentcomponent;
+
+	// Init vars from $_REQUEST[]
+	$action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
+	$extdisplay = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
+	
+	// Don't display this stuff it it's on a 'This xtn has been deleted' page.
+	if ($action != 'del') {
+		$langcode = languages_user_get($extdisplay);
+
+		$section = _('Language');
+		$msgInvalidLanguage = _('Please enter a valid Language Code');
+		$currentcomponent->addguielem($section, new gui_textbox('langcode', $langcode, _('Language Code'), _('The language code for this user. This will result in messages such as voiclangcode prompts to use the selected language if installed.'), "!isAlphanumeric()", $msgInvalidLanguage, true));
+	}
+}
+
+function languages_configprocess() {
+	//create vars from the request
+	$action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
+	$ext = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
+	$extn = isset($_REQUEST['extension'])?$_REQUEST['extension']:null;
+	$langcode = isset($_REQUEST['langcode'])?$_REQUEST['langcode']:null;
+
+	if ($ext==='') { 
+		$extdisplay = $extn; 
+	} else {
+		$extdisplay = $ext;
+	} 
+	if ($action == "add" || $action == "edit") {
+		languages_user_update($extdisplay, $langcode);
+	} elseif ($action == "del") {
+		languages_user_del($extdisplay);
+	}
+}
+
+function languages_user_get($xtn) {
+	global $astman;
+
+	// Retrieve the language configuraiton from this user from ASTDB
+	$langcode = $astman->database_get("AMPUSER",$xtn."/language");
+
+	return $langcode;
+}
+
+function languages_user_update($ext, $langcode) {
+	global $astman;
+	
+	if ($ena === 'disabled') {
+		languages_user_del($ext);
+	} else {
+		// Update the settings in ASTDB
+		$astman->database_put("AMPUSER",$ext."/language",$langcode);
+	}
+}
+
+function languages_user_del($ext) {
+	global $astman;
+
+	// Clean up the tree when the user is deleted
+	$astman->database_deltree("AMPUSER/$ext/language");
+}
+
+function languages_check_destinations($dest=true) {
+	global $active_modules;
+
+	$destlist = array();
+	if (is_array($dest) && empty($dest)) {
+		return $destlist;
+	}
+	$sql = "SELECT language_id, dest, description FROM languages ";
+	if ($dest !== true) {
+		$sql .= "WHERE dest in ('".implode("','",$dest)."')";
+	}
+	$results = sql($sql,"getAll",DB_FETCHMODE_ASSOC);
+
+	$type = isset($active_modules['languages']['type'])?$active_modules['languages']['type']:'setup';
+
+	foreach ($results as $result) {
+		$thisdest = $result['dest'];
+		$thisid   = $result['language_id'];
+		$destlist[] = array(
+			'dest' => $thisdest,
+			'description' => 'Language Change: '.$result['description'],
+			'edit_url' => 'config.php?display=languages&type='.$type.'&extdisplay='.urlencode($thisid),
+		);
+	}
+	return $destlist;
+}
 ?>
--- uninstall.php
+++ uninstall.php
@@ -1,17 +1,9 @@
 <?php
 
 global $db;
-global $amp_conf;
 
-$miscapps_arr = miscapps_list();
-foreach ($miscapps_arr as $item) {
-	echo "removing ".$item['description']."..";
-	miscapps_delete($item['miscapps_id']);
-	echo "done<br>\n";
-}
-
-echo "dropping table miscapps..";
-sql("DROP TABLE IF EXISTS `miscapps`");
+echo "dropping table languages..";
+sql("DROP TABLE IF EXISTS `languages`");
 echo "done<br>\n";
 
 ?>
