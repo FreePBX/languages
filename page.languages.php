3cdf90997e123e77c167be6ffc6860ba89185247
Moshe Brevda <mbrevda@gmail.com>
2008-07-06 01:58:08 +0000

    #2845 add tabindex to all items except those called via drawlistmenu or drawselect or other hooks. also missed page.routing.php

--- page.languages.php
+++ page.languages.php
@@ -87,11 +87,11 @@ echo $helptext;
 	<tr><td colspan="2"><h5><?php  echo ($extdisplay ? _("Edit Language Instance") : _("Add Language Instance")) ?><hr></h5></td></tr>
 	<tr>
 		<td><a href="#" class="info"><?php echo _("Description")?>:<span><?php echo _("The descriptive name of this language instance. For example \"French Main IVR\"")?></span></a></td>
-		<td><input size="30" type="text" name="description" value="<?php  echo $description; ?>"></td>
+		<td><input size="30" type="text" name="description" value="<?php  echo $description; ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
 	<tr>
 		<td><a href="#" class="info"><?php echo _("Language Code")?>:<span><?php echo _("The Asterisk language code you want to change to. For example \"fr\" for French, \"de\" for German")?></span></a></td>
-		<td><input size="14" type="text" name="lang_code" value="<?php echo $lang_code; ?>" /></td> </tr>
+		<td><input size="14" type="text" name="lang_code" value="<?php echo $lang_code; ?>"  tabindex="<?php echo ++$tabindex;?>"/></td> </tr>
 	<tr><td colspan="2"><br><h5><?php echo _("Destination")?>:<hr></h5></td></tr>
 
 <?php 
@@ -100,7 +100,7 @@ echo drawselects($dest,0);
 ?>
 			
 	<tr>
-		<td colspan="2"><br><input name="Submit" type="submit" value="<?php echo _("Submit Changes")?>">
+		<td colspan="2"><br><input name="Submit" type="submit" value="<?php echo _("Submit Changes")?>" tabindex="<?php echo ++$tabindex;?>">
 			<?php if ($extdisplay) { echo '&nbsp;<input name="delete" type="submit" value="'._("Delete").'">'; } ?>
 		</td>		
 
