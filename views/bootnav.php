33c3f69bc3ff768fbb940c0ff5e7a1184aeb8572
Megha BM <meghabm@sangoma.com>
2022-10-18 09:26:46 +0000

    FREEI-4928 Fixed typo issue

--- views/bootnav.php
+++ views/bootnav.php
@@ -1,5 +1,5 @@
 <div id="toolbar-langrnav">
-<a href="config.php?display=languages" class="btn btn-default"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Languagess") ?></a>
+<a href="config.php?display=languages" class="btn btn-default"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Languages") ?></a>
 <a href="config.php?display=languages&view=form" class="btn btn-default" ><i class="fa fa-plus"></i>&nbsp; <?php echo _("Add Language") ?></a>
 </div>
 <table data-toolbar="#toolbar-langrnav" data-escape="true" data-url="ajax.php?module=languages&amp;command=getJSON&amp;jdata=grid" data-cache="false" data-toggle="table" data-search="true" class="table" id="table-all-side">
